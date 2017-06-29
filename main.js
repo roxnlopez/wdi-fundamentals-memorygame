81001e98653b8249bed43425643de2490ccd6d96
roxnlopez <roxnlopez@gmail.com>
2017-06-28 18:29:43 -0600

    redid all the js except unit 11

--- main.js
+++ main.js
@@ -2,67 +2,60 @@ console.log("Up and running!");
 
 var cards = [
 	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
-	},
-	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
-	},
-	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
-	},
-	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
-	}
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+   },
+   {
+       rank: "queen",
+       suit: "diamonds",
+       cardImage: "images/queen-of-diamonds.png"
+   },
+   {
+       rank: "king",
+       suit: "hearts",
+       cardImage: "images/king-of-hearts.png"
+   },
+   {
+       rank: "king",
+       suit: "diamonds",
+       cardImage: "images/king-of-diamonds.png"
+   }
 ];
-
-console.log("Hello World");
 
 var cardsInPlay = [];
 
-
-var checkForMatch = function(){
-	this.setAttribute('src', cards[cardId].cardImage);
-var checkForMatch = function(){
-	console.log(cards[cardId].cardImage);
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function () {
+	this.setAttribute(cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {	
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+}
 };
-};
-console.log("Roxann is my name");
-
-var flipCard = function(){
-	this.getAttribute('data-id');
-	checkForMatch();
+//console.log("Roxann is my name");
+var flipCard = function () {
+	this.getAttribute('data-id');	
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);	
-	console.log(cards.cardImage);
-	console.log(cards.suit);
-	document.addEventListener('click', cardElement);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
 };
-console.log("JS is hard");
+
 var createBoard = function () {
-	for (var i=0; i<cards.length; i++) {
+	for (var i=0; i<cards.length; i++) {	
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
-		cardElement.setAttribute('src', "images/back.png");
-		cardElement.setAttribute('data-id', i);
-		document.addEventListener('click', cardElement);
-		document.getElementById('game-board').appendChild(cardElement);
+		appendChild(cardElement);
+		addEventListener('click', 'flipCard');
 	}
 };
+
 createBoard();
-console.log("Roxann is my name");
+
 
 
